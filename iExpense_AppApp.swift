599183821f4947bd26ef55f4597ab3a82f2c6805
asluis <asluisinfo@gmail.com>
2020-12-20 13:06:26 -0800

    Initial upload

---
+++ iExpense_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_AppApp.swift
+//  iExpense App
+//
+//  Created by Luis Alvarez on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
