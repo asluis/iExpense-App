dfffbaa52fda201350a51a49a9496b46021c84d2
asluis <asluisinfo@gmail.com>
2020-12-24 11:07:17 -0800

    Build UI for adding an expense. No functionality yet

--- AddExpenseView.swift
+++ AddExpenseView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct AddExpense_UI: View {
+    
+    @ObservedObject var expenses:Expenses
+    
+    @State var price = ""
+    @State var transactionName = ""
+    @State var transSelection = 0
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    let transOptions = ["Personal", "Business"]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Form{
+                Section(header: Text("Add transaction details")){
+                    TextField("Transaction Price", text: $price)
+                    TextField("Transaction Name", text: $transactionName)
+                }
+                
+                Section(header: Text("Select transaction type")){
+                    Picker("", selection: $transSelection){
+                        ForEach(transOptions, id: \.self) { type in
+                            Text(type)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+            }
+            .navigationTitle("Add New Expense")
+            .navigationBarItems(leading:
+                Button(action: {
+                    //TODO: Reset state
+                    
+                    presentationMode.wrappedValue.dismiss()
+                }){
+                    Image(systemName: "arrowshape.turn.up.backward.fill")
+                }
+            ,trailing:
+                Button(action: {
+                    // TODO: Perform action of adding expense to list
+                    presentationMode.wrappedValue.dismiss()
+                }){
+                    Image(systemName: "plus")
+                }
+            )
+        }
     }
 }
 
 struct AddExpense_UI_Previews: PreviewProvider {
     static var previews: some View {
-        AddExpense_UI()
+        AddExpense_UI(expenses: Expenses())
     }
 }
