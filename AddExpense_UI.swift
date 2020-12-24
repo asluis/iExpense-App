7aba752eda2af6d61ed39e8e7450b0a930522a42
asluis <asluisinfo@gmail.com>
2020-12-23 16:38:11 -0800

    Created file that will handle the UI when user adds an entry

---
+++ AddExpense_UI.swift
@@ -0,0 +1,20 @@
+//
+//  AddExpense_UI.swift
+//  iExpense App
+//
+//  Created by Luis Alvarez on 12/23/20.
+//
+
+import SwiftUI
+
+struct AddExpense_UI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddExpense_UI_Previews: PreviewProvider {
+    static var previews: some View {
+        AddExpense_UI()
+    }
+}
